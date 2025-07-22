cc82f95d144aa4c1684c56bbb55cf2be07858fe9
DataBook4U (Sebastian) <opocensky.sebastian@outlook.de>
2025-07-22 18:46:47 +0200

    tried out some solutions

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -27,13 +27,24 @@ class solution:
     #I think of iterating through the string and adding substrings to a list. Then the substring in the list with the highest len() value gets returned
     #Update: it might be better using a set instead of a list
     def lengthOfLongestSubstring(self):
-        substrings = set()
-        start = 0
-        max_l = len(self.input)
+        substrings = []
+        sequences = []
+        i = 0
+        j = 1
+        length = len(self.input)
 
-        for letter in self.input:
-            print(letter)
+        #Place string in array
+        while i < length:
+            substrings.append(self.input[i])
+            i += 1
 
+        #Determine sequences in array
+        for i in range(0, length):
+            substrings = "".join(substrings)
+            sequences.append(substrings)
+
+
+        return sequences
 
 
 s = "abcabcbb"
